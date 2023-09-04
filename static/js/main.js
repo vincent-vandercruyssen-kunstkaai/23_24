4a6c9bf18114620878d9dee87918578b1dc2fb89
vincent-vandercruyssen-kunstkaai <vincent.vandercruyssen@so.antwerpen.be>
2023-09-04 16:30:43 +0200

    first commit

---
+++ static/js/main.js
@@ -0,0 +1,17 @@
+$('header').after('<button id="themetoggle"></button>');
+
+$("#themetoggle").click(function () {
+    $("body").toggleClass("lightmode");
+});
+
+$(".noslides section").after("<hr>");
+
+function calcAndShowTotal() {
+    var total = 0;
+    $(':checked').each(function () {
+        total++;
+    });
+    $('.total').val(total);
+}
+
+$(':checkbox').change(calcAndShowTotal);
